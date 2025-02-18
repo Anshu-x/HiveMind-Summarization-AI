755286b74e8c8391b6a20f5985907ff08af35b54
Anshu-x <ansumanreet333@gmail.com>
2025-02-18 21:37:05 +0530

    Added py command in startup shell to check nltk data path

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 echo "Downloading NLTK data..."
 python -c "import nltk; nltk.download('punkt'); print('NLTK punkt downloaded successfully')"
+python -c "import nltk; print(nltk.data.path)"
 
 echo "Starting Flask app..."
 exec python app.py
