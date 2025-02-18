80f64fd2dfa930eda9f7742f46878936fd3df307
Anshu-x <ansumanreet333@gmail.com>
2025-02-18 20:57:22 +0530

    Updated the app.py file to dynamically use the port

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.text_rank import TextRankSummarizer
+import os
+
 
 app = Flask(__name__)
 
@@ -37,5 +39,6 @@ def summarize_endpoint():
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
